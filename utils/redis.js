3ff933e7a876dec50a0d47ff93622662e7b83731
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-09-05 01:45:21 +0100

    Solution to question 1

---
+++ utils/redis.js
@@ -0,0 +1,47 @@
+// This file contains class that handles redis operations
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.redis = createClient();
+    this.connected = true;
+    this.redis.on('error', (err) => {
+      if (err) {
+        this.connected = false;
+      }
+    });
+
+    this.redis.on('connect', () => {
+      this.connected = true;
+    });
+  }
+
+  isAlive() {
+    return this.connected;
+  }
+
+  async get(key) {
+    // const value = await this.redis.get(key);
+    // return value;
+    const getValue = promisify(this.redis.get).bind(this.redis);
+    const value = await getValue(key);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    // setex takes in expiration(in this case duration) argument
+    // await this.redis.setex(key, duration, value);
+    const setValue = promisify(this.redis.set).bind(this.redis);
+    await setValue(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    // await this.redis.del(key);
+    const delValue = promisify(this.redis.del).bind(this.redis);
+    await delValue(key);
+  }
+}
+
+export const redisClient = new RedisClient();
+export default redisClient;
